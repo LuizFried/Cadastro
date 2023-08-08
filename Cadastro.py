b56f1190d19425bd0fff35195f601d759eb0b00d
LuizFried <luifried50@gmail.com>
2023-08-08 07:01:30 -0300

    Update Cadastro.py

--- Cadastro.py
+++ Cadastro.py
@@ -7,7 +7,7 @@ from tkinter import messagebox
 janela = tk.Tk()
 janela.title('Registro')
 janela.geometry('600x500')
-janela.resizable(False, False)
+janela.resizable(True, True)
 janela.configure(background='#002368')
 
 def mail(mail=''):
@@ -32,13 +32,36 @@ def mail(mail=''):
     except:
         messagebox.showerror('ERRO', 'Digite um email válido')
 
-f0 = tk.Frame(janela,background='#a0ded6')
-f0.grid(row=0, column=0, columnspan=3,padx=10, pady=10)
 
-texto1 = ctk.CTkLabel(f0, text='EMAIL:',width=150, corner_radius=100,fg_color='black',text_color='white',
-                      bg_color='#a0ded6', font=('Arial', 14, 'bold'))
-texto1.grid(row=0, column=0, padx=2, pady=10)
-teste = tk.LabelFrame(janela,text='testando')
-teste.grid(row=1,column=1)
+frame1 = tk.Frame(janela, background='#a0ded6', width=400,height=200)
+frame1.grid(padx=10, pady=10,columnspan=5)
+
+
+
+nome = ctk.CTkLabel(frame1, text='NOME:',width=50, corner_radius=1000,fg_color='black',text_color='white',
+                      bg_color='#a0ded6', font=('Arial', 12, 'bold'))
+nome.grid(row=0, column=0, padx=2, pady=10)
+
+enome = ctk.CTkEntry(frame1,width=200)
+enome.grid(row=0, column=1, padx=20, pady=10)
+
+
+idade = ctk.CTkLabel(frame1, text='IDADE:',width=50, corner_radius=1000,fg_color='black',text_color='white',
+                      bg_color='#a0ded6', font=('Arial', 12, 'bold'))
+idade.grid(row=0, column=3, padx=20, pady=10)
+
+eidade = ctk.CTkEntry(frame1,width=35)
+eidade.grid(row=0, column=4, padx=20, pady=10)
+
+
+email = ctk.CTkLabel(frame1, text='EMAIL:',width=50, corner_radius=1000,fg_color='black',text_color='white',
+                      bg_color='#a0ded6', font=('Arial', 12, 'bold'))
+email.grid(row=3, column=0, padx=2, pady=10)
+
+eemail = ctk.CTkEntry(frame1,width=200)
+eemail.grid(row=3, column=1, padx=10, pady=10)
+
+
+
 
 janela.mainloop()
